dab204aedc5ecc8b744dd69ec48e2927b4d15d5c
Golla Vishnu <vishnugolla46@gmail.com>
2026-08-29 19:10:34 +0530

    api updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://student-mental-health-prediction-using.onrender.com/";
+  const API_BASE = "https://student-mental-health-prediction-using.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
